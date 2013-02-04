8da5744e9d73d8eafb3feb46b18087325a133856
ArnauAregall <arnau@ar3soft.com>
2013-02-04 15:51:18 +0100

    - Refactored MainActivity.java

--- src/views/MainActivity.java
+++ src/views/MainActivity.java
@@ -9,14 +9,11 @@ import controllers.EmployeeDataSource;
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
 import android.app.ListActivity;
-import android.content.Intent;
 
 /**
  * MainActivity, first activity that will be shown. 
- * Connects to the SQLite database and retrieves a list of employee into a ListView with custom views.
+ * Connects to the SQLite database and retrieves a list of employees into a ListView with custom views.
  * @author ArnauAregall
  *
  */
@@ -25,6 +22,7 @@ public class MainActivity extends ListActivity {
 	EmployeeAdapter adapter = null;
 	List<Employee> employees = null;
 	EmployeeDataSource employeeDS = null;
+	
 	/**
 	 * onCreate method
 	 */
@@ -34,9 +32,12 @@ public class MainActivity extends ListActivity {
 		
 		employeeDS = new EmployeeDataSource(this);
 		employeeDS.open(true); // open DB connection
+		
 		employees = employeeDS.getEmployees(); // retrieve data from DB
+		
 		adapter = new EmployeeAdapter(this, employees, employeeDS);
 		adapter.notifyDataSetChanged();
+		
 		this.setListAdapter(adapter);
 	}
 		
