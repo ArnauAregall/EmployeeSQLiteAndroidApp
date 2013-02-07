b928a02b1c13bacd7efbac891db4a9975512ce5d
ArnauAregall <arnau@ar3soft.com>
2013-02-07 02:19:49 +0100

    - Added JavaDoc to EmployeeUpdate.java

--- src/views/EmployeeUpdate.java
+++ src/views/EmployeeUpdate.java
@@ -29,16 +29,23 @@ public class EmployeeUpdate extends Activity implements OnClickListener{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_employee_update);
+		
+		// capture Employee id from EmployeeDetails to load its data
 		Bundle bundle = getIntent().getExtras();
-		empId = bundle.getInt("employee_id");
+		empId = bundle.getInt("employee_id"); 
+		
 		employeeDS = new EmployeeDataSource(this);
 		employeeDS.open(true); // open DB connection in writable mode, because of Delete option
+		// get Employee object by ID
 		employee = employeeDS.getEmployee(empId);
+		
 		fillEmployeeData(employee);
-		System.out.println(employee.getId());
 		validator = new EmployeeValidator(this);
 	}
-	
+	/**
+	 * Fills Employee data from database into the view in order to get modified.
+	 * @param employee
+	 */
 	private void fillEmployeeData(Employee employee) {
 		tvTitle = (TextView) findViewById(R.id.update_title);
 		tvTitle.setText(getResources().getString(R.string.update)+ " " + employee.getName());
@@ -67,6 +74,9 @@ public class EmployeeUpdate extends Activity implements OnClickListener{
 		btnSave.setOnClickListener(this);
 	}
 	
+	/**
+	 * Makes sure every value of every EditText is valid and calls updateEmployee
+	 */
 	public void onClick(View v) {
 		if(v.equals(btnSave)) {
 			// eval EditText values in this order
@@ -82,6 +92,11 @@ public class EmployeeUpdate extends Activity implements OnClickListener{
 		}
 	}
 	
+	/**
+	 * Updates an Employee register into the database 
+	 * and prompts AlertDialog that notifies the update.
+	 * @param employee, the Employee with updated attributes
+	 */
 	private void updateEmployee(Employee employee) {
 		int updated = employeeDS.updateEmployee(employee);
 		String alertMessage = "";
